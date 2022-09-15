53200c342c38301f3358a767451b91d56bbd0c06
Raselmahamud <60851442+Raselmahamud@users.noreply.github.com>
2022-09-16 00:14:33 +0600

    2D game
    almost done the code.

---
+++ gameprojecct.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <conio.h>
+#include<windows.h>
+char square[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+int checkwin();
+void board();
+
+int main()
+
+{
+    printf( "Hello\n");
+
+system("color 5C");
+    Beep(1000,1000);
+    printf("This is Our 1st Project\n ");
+    system("color 6F");
+    Beep(500,1000);
+    printf("WELCOME TO OUR GAME\n");
+   system("color 1C");
+    Beep(800,500);
+    printf("ENJOY IT\n\n");
+    system("color 8D");
+    Beep(1200,500);
+    system("color 7C");
+    Beep(400,800);
+    system("color 3F");
+
+
+
+
+    int player = 1, i, choice;
+    Beep(500,500);
+
+    char mark;
+    do
+    {
+        board();
+        player = (player % 2) ? 1 : 2;
+
+        printf("Player %d, enter a number:  ", player);
+        scanf("%d", &choice);
+
+        mark = (player == 1) ? 'X' : 'O';
+        Beep(500,400);
+
+        if (choice == 1 && square[1] == '1')
+            square[1] = mark;
+
+        else if (choice == 2 && square[2] == '2')
+            square[2] = mark;
+
+        else if (choice == 3 && square[3] == '3')
+            square[3] = mark;
+
+        else if (choice == 4 && square[4] == '4')
+            square[4] = mark;
+
+        else if (choice == 5 && square[5] == '5')
+            square[5] = mark;
+
+        else if (choice == 6 && square[6] == '6')
+            square[6] = mark;
+
+        else if (choice == 7 && square[7] == '7')
+            square[7] = mark;
+
+        else if (choice == 8 && square[8] == '8')
+            square[8] = mark;
+
+        else if (choice == 9 && square[9] == '9')
+            square[9] = mark;
+
+        else
+        {
+            printf("Invalid move ");
+
+            player--;
+            getch();
+        }
+        i = checkwin();
+
+        player++;
+    }
+    while (i ==  - 1);
+
+    board();
+
+    if (i == 1)
+        printf("==>\aPlayer %d win ", --player);
+    else
+        printf("==>\aGame draw");
+        Beep(500,500);
+        Beep(1000,1000);
+        Beep(800,600);
+        Beep(1000,400);
+
+    getch();
+
+    return 0;
+}
+
+
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+        return 1;
+
+    else if (square[4] == square[5] && square[5] == square[6])
+        return 1;
+
+    else if (square[7] == square[8] && square[8] == square[9])
+        return 1;
+
+    else if (square[1] == square[4] && square[4] == square[7])
+        return 1;
+
+    else if (square[2] == square[5] && square[5] == square[8])
+        return 1;
+
+    else if (square[3] == square[6] && square[6] == square[9])
+        return 1;
+
+    else if (square[1] == square[5] && square[5] == square[9])
+        return 1;
+
+    else if (square[3] == square[5] && square[5] == square[7])
+        return 1;
+
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
+             square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7]
+             != '7' && square[8] != '8' && square[9] != '9')
+
+        return 0;
+    else
+        return  - 1;
+}
+
+
+
+
+void board()
+{
+    system("cls");
+    printf(" **** WELCOME TO **** ");
+    printf("\n\n\ ==>Tic Tac Toe<==\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
+
+    printf("     |     |     \n\n");
+
+    return 0;
+}
+
